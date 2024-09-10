97c04b34cc933a9cc4bc3da716299f3bf1d6b0dd
임성호 <tjd4119@dyphi.com>
2024-09-10 13:58:29 +0900

    [update] 자바 변수 강의 문제풀이 추가

---
+++ javaStudy/src/var/varEx3.java
@@ -0,0 +1,12 @@
+package var;
+
+public class varEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+    }
+}
