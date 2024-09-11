8fe8d5dbad4b9a432a183d31c3c79a9cf403a2f3
임성호 <tjd4119@dyphi.com>
2024-09-11 18:30:09 +0900

    [update] java basic array

---
+++ javaStudy/src/basic/study6/array/ArrayEx3.java
@@ -0,0 +1,32 @@
+package basic.study6.array;
+
+import java.util.Scanner;
+
+public class ArrayEx3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = 5;
+        int[] numbers = new int[length];
+        System.out.println(length + "개의 정수를 입력하세요.");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int maxNumber = numbers[0];
+        int minNumber = numbers[0];
+
+        for (int number : numbers) {
+            if (number > maxNumber) {
+                maxNumber = number;
+            }
+            if (number < minNumber) {
+                minNumber = number;
+            }
+        }
+        System.out.println("가장 작은 정수: " + minNumber);
+        System.out.println("가장 큰 정수: " + maxNumber);
+
+    }
+}
+
