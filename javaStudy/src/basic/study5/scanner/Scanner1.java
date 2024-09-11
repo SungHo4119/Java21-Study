dfb35c907c63e36a37e311bbcfcde9e3d20311b4
임성호 <tjd4119@dyphi.com>
2024-09-11 15:17:05 +0900

    [update] java basic study5 scanner

---
+++ javaStudy/src/basic/study5/scanner/Scanner1.java
@@ -0,0 +1,21 @@
+package basic.study5.scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("문자열을 입력하세요 : ");
+        String str = scanner.nextLine();
+        System.out.println("입력한 문자열 : " + str);
+
+        System.out.print("정수를 입력하세요 : ");
+        int valueInt = scanner.nextInt();
+        System.out.println("입력한 정수 : " + valueInt);
+
+        System.out.print("실수를 입력하세요 : ");
+        double valueDouble = scanner.nextDouble();
+        System.out.println("입력한 실수 : " + valueDouble);
+    }
+}
