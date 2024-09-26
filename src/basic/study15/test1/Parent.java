82d958da3dfaaa2236481401a492cc0635807e3a
SungHo <tjd4119@naver.com>
2024-09-26 09:36:46 +0900

    java Study Polymorphism

---
+++ src/basic/study15/test1/Parent.java
@@ -0,0 +1,9 @@
+package basic.study15.test1;
+
+public class Parent {
+    public String value = "Parent Value";
+
+    public void print() {
+        System.out.println("Parent Method - Value : " + this.value);
+    }
+}
---
+++ src/basic/study15/test1/PolymorphismMain.java
@@ -0,0 +1,32 @@
+package basic.study15.test1;
+
+public class PolymorphismMain {
+
+    public static void main(String[] args) {
+        Parent p = new Parent();
+
+        System.out.println(p.value);
+        p.print();
+
+        Child c = new Child();
+        System.out.println(c.value);
+        c.print();
+
+        Grandson g = new Grandson();
+        System.out.println(g.value);
+        g.print();
+
+        // 다형성 테스트
+        Parent p1 = new Child();
+        System.out.println(p1.value);
+        p1.print();
+
+        Parent p2 = new Grandson();
+        System.out.println(p2.value);
+        p2.print();
+
+        Child c1 = new Grandson();
+        System.out.println(c1.value);
+        c1.print();
+    }
+}
