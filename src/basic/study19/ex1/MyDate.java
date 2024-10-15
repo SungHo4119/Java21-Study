9d398061c7d7bf96ec2dd3e08a0c1e607b1c50e4
SungHo <tjd4119@naver.com>
2024-10-15 20:06:01 +0900

    [update] JavaStudy 중급 1편 불변 객체 문제

---
+++ src/basic/study19/ex1/MyDate.java
@@ -0,0 +1,31 @@
+package basic.study19.ex1;
+
+public class MyDate {
+
+  private int year;
+  private int month;
+  private int day;
+
+  public MyDate(int year, int month, int day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public void setMonth(int month) {
+    this.month = month;
+  }
+
+  public void setDay(int day) {
+    this.day = day;
+  }
+
+  @Override
+  public String toString() {
+    return year + "-" + month + "-" + day;
+  }
+}
